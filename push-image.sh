dc2951f2de5e9246cca49e72ee106d39deee6d30
Petr Praus <392315+praus@users.noreply.github.com>
2019-11-27 16:25:50 -0800

    push-image: use gradlew, use explicit workdir, add comment
    * push-image now makes it clear at the top what does it do (manually
    pushes a container into a registry).
    * push-image now uses gradle wrapper instead of relying on the fact that
    Gradle is installed system-wide.
    * The script now detects the directory it is stored in and assumes
    Gradle root is at the same level as this script. push-image changes to
    that directory before invoking Gradle to ensure Gradle is invoked from
    its root directory.
    * push-image no longer tries to invoke cdk and deploy infrastructure.

--- push-image.sh
+++ push-image.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euf -o pipefail  # Makes bash behave sanely, see https://sipb.mit.edu/doc/safe-shell/
 
+# This script can be used to manually push a container into a registry.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 txtblk='\e[0;30m' # Black - Regular
 txtred='\e[0;31m' # Red
 txtgrn='\e[0;32m' # Green
@@ -22,16 +25,15 @@ image_name="com.amazonaws.fcj/faster-cryptography-in-java"
 repo_name="com.amazonaws.fcj/faster-cryptography-in-java-$stage"
 
 $(aws ecr get-login --no-include-email)
-gradle clean
-gradle docker
+
+pushd "$DIR" > /dev/null 2>&1
+./gradlew clean docker
+popd > /dev/null 2>&1
 
 tag="latest"
 
 repo_uri="$(aws ecr describe-repositories --repository-names "$repo_name"| jq -r '.repositories[0]["repositoryUri"]')"
 repo_uri_with_tag="$repo_uri:$tag"
 docker tag "$image_name":"$tag" "$repo_uri_with_tag"
-
-# Ensure the ECR repo has been created
-cdk deploy fcj-deployment-infra --context "stage=$stage"
 
 docker push "$repo_uri_with_tag"
