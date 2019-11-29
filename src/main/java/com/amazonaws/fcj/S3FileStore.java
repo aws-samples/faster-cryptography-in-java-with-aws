cfc5fde11e57dc2c750f7b26ded43e208ce673e3
Petr Praus <392315+praus@users.noreply.github.com>
2019-11-29 12:05:10 -0800

    S3FileStore: limit upload part parallelism
    We were too eager in prefetching parts to upload which caused us to
    buffer much more data than we should. See comments in the code for more
    details.

--- src/main/java/com/amazonaws/fcj/S3FileStore.java
+++ src/main/java/com/amazonaws/fcj/S3FileStore.java
@@ -11,7 +11,6 @@ import com.amazonaws.encryptionsdk.AwsCrypto;
 import com.amazonaws.encryptionsdk.CryptoOutputStream;
 import com.amazonaws.encryptionsdk.kms.KmsMasterKey;
 import com.amazonaws.encryptionsdk.kms.KmsMasterKeyProvider;
-import com.google.common.util.concurrent.Futures;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -42,6 +41,7 @@ import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.unit.DataSize;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
@@ -73,7 +73,21 @@ public class S3FileStore implements FileStore {
     private static final String S3_OBJECT_KEY_PREFIX = "files/";
     private static final int ENCRYPTION_BUFFER_SIZE_BYTES = 16_384;
 
-    private static final int BUFFER_THRESHOLD_BYTES = 6 * 1024 * 1024;
+    private static final DataSize BUFFER_THRESHOLD = DataSize.ofMegabytes(6);
+
+    /**
+     * Sets the maximum number of parallel part uploads for a single request.
+     */
+    private static final int UPLOAD_PART_PARALLELISM = 4;
+
+    /**
+     * When a new sub-flux for upload part starts, it needs to send a request for more "data events" downstream so that
+     * it has something to process (this is called prefetch). Reactor uses 32 as the default prefetch number. Since our
+     * data events are BUFFER_THRESHOLD large (6 MB at the time I'm writing this), it means each upload part sub-flux
+     * would have 192 MB of data prefetched. That's quite excessive so we're capping it at just one prefetched data
+     * event.
+     */
+    private static final int UPLOAD_PART_PARALLEL_PREFETCH = 1;
 
     private final Scheduler scheduler = Schedulers.parallel();
 
@@ -130,12 +144,12 @@ public class S3FileStore implements FileStore {
         encryptingOutputStream.setMaxInputLength(contentLength);
 
         final String objectPath = S3_OBJECT_KEY_PREFIX + fileMetadata.getId();
-        final CompletableFuture<CreateMultipartUploadResponse> uploadInitFuture =
+        final Mono<CreateMultipartUploadResponse> uploadInitMono = Mono.fromFuture(
                 s3.createMultipartUpload(CreateMultipartUploadRequest.builder()
                                                  .bucket(fileBucketName)
                                                  .key(objectPath)
                                                  .contentType(fileMetadata.getContentType())
-                                                 .build());
+                                                 .build()));
 
         final AtomicInteger partCounter = new AtomicInteger(0);
         final AtomicLong timeSpentWithCryptoNanos = new AtomicLong(0);
@@ -154,22 +168,22 @@ public class S3FileStore implements FileStore {
                 // simplicity we're using S3 UploadPart API for all data and that API requires that all but the last
                 // part is at least 5 MB large so we'll buffer the encrypted chunks into a list until a threshold is
                 // reached.
-                .bufferUntil(new SizeMoreThan<>(BUFFER_THRESHOLD_BYTES, arr -> arr.length))
+                .bufferUntil(new SizeMoreThan<>(BUFFER_THRESHOLD.toBytes(), arr -> arr.length))
                 // Concatenate all the chunks from the list into one large byte array.
                 .map(Utils::concat)
-                // Split this flux of PartUpload structures into CPU_CORES sub-fluxes and sequentially start
-                // initiating uploads for items on each. Suppose we have 80 parts and 8 CPU cores. This flux will be
-                // split into 8 sub-fluxes and parts coming through will be distributed evenly into each sub-flux.
-                // Each sub-flux behaves synchronously: it will not start uploading the next part until the previous
-                // one is finished.
-                .parallel().runOn(scheduler)
-                .concatMap(encryptedPart -> uploadPart(encryptedPart, uploadInitFuture, partCounter))
+                // Wait until the CreateMultiPartUpload call finishes. We can't start uploading parts before then.
+                .delayUntil(part -> uploadInitMono)
+                // Split this flux of PartUpload structures into UPLOAD_PART_PARALLELISM rails (sub-fluxes) and
+                // sequentially start initiating uploads for items on each rail.
+                .parallel(UPLOAD_PART_PARALLELISM, UPLOAD_PART_PARALLEL_PREFETCH).runOn(scheduler)
+                .concatMap(encryptedPart -> initiatePartUpload(encryptedPart, uploadInitMono, partCounter))
                 // Collect all the ongoing chunk uploads into a list. This basically means the flux will wait until
                 // all part uploads were initiated.
                 .sequential()
+                // This is where we pause until all part uploads are finished.
                 .collectList()
                 // Now that we have all the completed parts, we'll complete the multi-part upload.
-                .flatMap(partUploads -> completeMultipartUpload(partUploads, uploadInitFuture, fileMetadata))
+                .flatMap(partUploads -> completeMultipartUpload(partUploads, uploadInitMono, fileMetadata))
                 .delayUntil(fm -> recordCryptoSpeed("encrypt", timeSpentWithCryptoNanos, plaintextBytesCounter));
     }
 
@@ -205,6 +219,7 @@ public class S3FileStore implements FileStore {
 
     /**
      * Delays <i>completion</i> of the {@code flux} until a given {@code delayTrigger} completes.
+     *
      * @param flux The flux to delay completion of.
      * @param delayTrigger The publisher that needs to complete before the flux will complete.
      * @param <T> Type of the elements in the flux we're delaying.
@@ -229,58 +244,63 @@ public class S3FileStore implements FileStore {
             // Looks like we have processed zero megabytes which is odd, let's not emit any speed metrics.
             return Mono.empty();
         }
-        final Duration timePerMb = Duration.ofNanos((long) ((double) timeSpentWithCryptoNanos.get() / megabytesProcessed));
+        final Duration timePerMb =
+                Duration.ofNanos((long) ((double) timeSpentWithCryptoNanos.get() / megabytesProcessed));
         LOG.info("Crypto speed metric: timeSpentWithCrypto={}, byteCount={}, timePerMb={}",
                  Duration.ofNanos(timeSpentWithCryptoNanos.get()), byteCount.get(), timePerMb);
         final String metricName = String.format("%s.duration.perMb", operation);
         return cloudWatchEmitter.putDurationMetricData(metricName, timePerMb).then();
     }
 
+    /**
+     * Completes the entire multipart upload and returns file metadata we'll give to the caller.
+     */
     private Mono<FileMetadata> completeMultipartUpload(
             final List<PartUpload> partUploads,
-            final CompletableFuture<CreateMultipartUploadResponse> uploadInitFuture,
+            final Mono<CreateMultipartUploadResponse> uploadInitFuture,
             final FileMetadata fileMetadata) {
-        final CreateMultipartUploadResponse uploadInitResp = Futures.getUnchecked(uploadInitFuture);
-        final String bucketName = uploadInitResp.bucket();
-        final String objectPath = uploadInitResp.key();
-        final String uploadId = uploadInitResp.uploadId();
+        return uploadInitFuture.flatMap(uploadInitResp -> {
+            final String bucketName = uploadInitResp.bucket();
+            final String objectPath = uploadInitResp.key();
+            final String uploadId = uploadInitResp.uploadId();
 
-        final MessageDigest overallChecksum = Utils.getMessageDigestForETag();
+            final MessageDigest overallChecksum = Utils.getMessageDigestForETag();
 
-        // We need to sort the list of uploads by part number. First, S3 requires the list of completed parts to be
-        // sorted by part number. Second, we need to hash them together in the right order!
-        partUploads.sort(Comparator.comparingInt(PartUpload::getPartNumber));
-        final List<CompletedPart> completedParts = new ArrayList<>(partUploads.size());
-        for (PartUpload partUpload : partUploads) {
-            completedParts.add(partUpload.asCompletedPart());
-            overallChecksum.update(partUpload.getPartChecksum());
-        }
+            // We need to sort the list of uploads by part number. First, S3 requires the list of completed parts to be
+            // sorted by part number. Second, we need to hash them together in the right order!
+            partUploads.sort(Comparator.comparingInt(PartUpload::getPartNumber));
+            final List<CompletedPart> completedParts = new ArrayList<>(partUploads.size());
+            for (PartUpload partUpload : partUploads) {
+                completedParts.add(partUpload.asCompletedPart());
+                overallChecksum.update(partUpload.getPartChecksum());
+            }
 
-        final CompleteMultipartUploadRequest completeUploadReq = CompleteMultipartUploadRequest.builder()
-                .bucket(bucketName)
-                .key(objectPath)
-                .uploadId(uploadId)
-                .multipartUpload(CompletedMultipartUpload.builder().parts(completedParts).build())
-                .build();
-        LOG.info("Completing multipart upload: bucketName={}, objectPath={}, uploadId={}, parts={}",
-                 bucketName, objectPath, uploadId, completedParts);
+            final CompleteMultipartUploadRequest completeUploadReq = CompleteMultipartUploadRequest.builder()
+                    .bucket(bucketName)
+                    .key(objectPath)
+                    .uploadId(uploadId)
+                    .multipartUpload(CompletedMultipartUpload.builder().parts(completedParts).build())
+                    .build();
+            LOG.info("Completing multipart upload: bucketName={}, objectPath={}, uploadId={}, parts={}",
+                     bucketName, objectPath, uploadId, completedParts);
 
-        return Mono.fromFuture(s3.completeMultipartUpload(completeUploadReq))
-                .handle((completedUpload, sink) -> {
-                    final String localEtag = createMultipartETag(overallChecksum.digest(), completedParts.size());
-                    final String remoteEtag = completedUpload.eTag();
-                    if (localEtag.equals(remoteEtag)) {
-                        LOG.info("Finished multipart upload of a new object: " +
-                                         "bucketName={}, objectPath={}, ETag={}",
-                                 bucketName, objectPath, localEtag);
-                        fileMetadata.setEtag(remoteEtag);
-                        sink.next(fileMetadata);
-                    } else {
-                        sink.error(new FcjServiceException(format(
-                                "Upload ETag mismatch: bucketName=%s, objectPath=%s, localETag=%s remoteETag=%s",
-                                bucketName, objectPath, localEtag, remoteEtag)));
-                    }
-                });
+            return Mono.fromFuture(s3.completeMultipartUpload(completeUploadReq))
+                    .handle((completedUpload, sink) -> {
+                        final String localEtag = createMultipartETag(overallChecksum.digest(), completedParts.size());
+                        final String remoteEtag = completedUpload.eTag();
+                        if (localEtag.equals(remoteEtag)) {
+                            LOG.info("Finished multipart upload of a new object: " +
+                                             "bucketName={}, objectPath={}, ETag={}",
+                                     bucketName, objectPath, localEtag);
+                            fileMetadata.setEtag(remoteEtag);
+                            sink.next(fileMetadata);
+                        } else {
+                            sink.error(new FcjServiceException(format(
+                                    "Upload ETag mismatch: bucketName=%s, objectPath=%s, localETag=%s remoteETag=%s",
+                                    bucketName, objectPath, localEtag, remoteEtag)));
+                        }
+                    });
+        });
     }
 
     private Flux<byte[]> closeCryptoStreamOnComplete(final Flux<byte[]> flux,
@@ -389,14 +409,63 @@ public class S3FileStore implements FileStore {
                         f, decryptingOutputStream, decryptedDataBuffer, timeSpentWithCryptoNanos));
     }
 
+    /**
+     * Kicks off the actual uploading of the part and returns a class that represents the process ({@link PartUpload}.
+     *
+     * @param partBytes The actual contents of the part. It's important that we lose the reference to this byte array
+     * when the part has been uploaded.
+     * @param uploadInitMono The {@link Mono} we need to get the response to CreateMultiPartUpload from S3.
+     * @param partCounter The atomic counter we use to keep track of parts.
+     * @return Another {@link Mono} containing state related to uploading this part.
+     */
+    private Mono<PartUpload> initiatePartUpload(
+            final byte[] partBytes,
+            final Mono<CreateMultipartUploadResponse> uploadInitMono,
+            final AtomicInteger partCounter) {
+        final int partNumber = partCounter.incrementAndGet();
+        return uploadInitMono
+                .map(uploadInitResp -> new PartUpload(uploadInitResp, partNumber, partBytes.length))
+                .flatMap(partUpload -> {
+                    LOG.info("Initiating {}", partUpload);
+                    final byte[] partChecksum = Utils.computeETagChecksum(partBytes);
+                    partUpload.setPartChecksum(partChecksum);
+                    final UploadPartRequest uploadPartRequest = partUpload.createUploadPartRequest();
+                    return Mono.fromFuture(s3.uploadPart(uploadPartRequest, new ByteArrayAsyncRequestBody(partBytes)))
+                            .handle((resp, sink) -> completePartUpload(resp, sink, partUpload));
+                });
+    }
+
+    /**
+     * This function is called when a part finishes uploading. It's primary function is to verify the ETag of the part
+     * we just uploaded.
+     */
+    private void completePartUpload(final UploadPartResponse uploadPartResponse,
+                                    final SynchronousSink<PartUpload> sink,
+                                    final PartUpload partUpload) {
+        final String partEtag = uploadPartResponse.eTag();
+        partUpload.setPartEtag(partEtag);
+        final String checksumEtag = createETag(partUpload.getPartChecksum());
+        if (checksumEtag.equals(partEtag)) {
+            LOG.info("Finished {}", partUpload);
+            sink.next(partUpload);
+        } else {
+            sink.error(new FcjServiceException(format(
+                    "ETag mismatch when uploading part %s, our ETag was %s but S3 returned %s",
+                    partUpload.getPartNumber(), checksumEtag, partEtag)));
+        }
+    }
+
+    /**
+     * A predicate to test how many bytes we have already accumulated.
+     */
     static class SizeMoreThan<T> implements Predicate<T> {
 
-        private final int thresholdBytes;
+        private final long thresholdBytes;
         private final Function<T, Integer> sizeSupplier;
 
-        private int byteCounter = 0;
+        private long byteCounter = 0;
 
-        SizeMoreThan(final int thresholdBytes, final Function<T, Integer> sizeSupplier) {
+        SizeMoreThan(final long thresholdBytes, final Function<T, Integer> sizeSupplier) {
             this.thresholdBytes = thresholdBytes;
             this.sizeSupplier = sizeSupplier;
         }
@@ -472,38 +541,6 @@ public class S3FileStore implements FileStore {
         }
     }
 
-    Mono<PartUpload> uploadPart(final byte[] partBytes,
-                                final CompletableFuture<CreateMultipartUploadResponse> uploadInitFuture,
-                                final AtomicInteger partCounter) {
-        final PartUpload partUpload = new PartUpload(uploadInitFuture, partCounter.incrementAndGet(), partBytes.length);
-        LOG.info("Initiating {}", partUpload);
-        final byte[] partChecksum = Utils.computeETagChecksum(partBytes);
-        partUpload.setPartChecksum(partChecksum);
-        final UploadPartRequest uploadPartRequest = partUpload.createUploadPartRequest();
-        return Mono.fromFuture(s3.uploadPart(uploadPartRequest, new ByteArrayAsyncRequestBody(partBytes)))
-                .handle((resp, sink) -> completePartUpload(resp, sink, partUpload));
-    }
-
-    /**
-     * This function is called when a part finishes uploading. It's primary function is to verify the ETag of the
-     * part we just uploaded.
-     */
-    private void completePartUpload(final UploadPartResponse uploadPartResponse,
-                                    final SynchronousSink<PartUpload> sink,
-                                    final PartUpload partUpload) {
-        final String partEtag = uploadPartResponse.eTag();
-        partUpload.setPartEtag(partEtag);
-        final String checksumEtag = createETag(partUpload.getPartChecksum());
-        if (checksumEtag.equals(partEtag)) {
-            LOG.info("Finished {}", partUpload);
-            sink.next(partUpload);
-        } else {
-            sink.error(new FcjServiceException(format(
-                    "ETag mismatch when uploading part %s, our ETag was %s but S3 returned %s",
-                    partUpload.getPartNumber(), checksumEtag, partEtag)));
-        }
-    }
-
     /**
      * Represents the state around uploading the part but not the actual content of the part.
      */
@@ -516,9 +553,8 @@ public class S3FileStore implements FileStore {
         private byte[] partChecksum;
         private String partEtag;
 
-        PartUpload(final CompletableFuture<CreateMultipartUploadResponse> uploadInitFuture,
+        PartUpload(final CreateMultipartUploadResponse uploadInitResp,
                    final int partNumber, final long partLength) {
-            final CreateMultipartUploadResponse uploadInitResp = Futures.getUnchecked(uploadInitFuture);
             this.bucketName = uploadInitResp.bucket();
             this.objectPath = uploadInitResp.key();
             this.uploadId = uploadInitResp.uploadId();
@@ -544,12 +580,12 @@ public class S3FileStore implements FileStore {
             this.partEtag = partEtag;
         }
 
-        public void setPartChecksum(final byte[] partChecksum) {
-            this.partChecksum = partChecksum;
-        }
-
         byte[] getPartChecksum() {
             return partChecksum;
+        }
+
+        public void setPartChecksum(final byte[] partChecksum) {
+            this.partChecksum = partChecksum;
         }
 
         CompletedPart asCompletedPart() {
