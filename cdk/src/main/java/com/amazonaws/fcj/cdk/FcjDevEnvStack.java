b0d9505f9aa0b65ae636382434ac7d0b5ca784df
Petr Praus <392315+praus@users.noreply.github.com>
2019-11-29 11:04:14 -0800

    CDK dev env: make Cloud9 instance name a CFN parameter

--- cdk/src/main/java/com/amazonaws/fcj/cdk/FcjDevEnvStack.java
+++ cdk/src/main/java/com/amazonaws/fcj/cdk/FcjDevEnvStack.java
@@ -5,21 +5,33 @@ package com.amazonaws.fcj.cdk;
 
 import java.time.Duration;
 
+import software.amazon.awscdk.core.CfnParameter;
+import software.amazon.awscdk.core.CfnParameterProps;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
 import software.amazon.awscdk.services.cloud9.CfnEnvironmentEC2;
 import software.amazon.awscdk.services.cloud9.CfnEnvironmentEC2Props;
 import software.amazon.awscdk.services.ec2.Vpc;
 
 public class FcjDevEnvStack extends Stack {
     public FcjDevEnvStack(final Construct scope, final String id) {
-        super(scope, id);
+        super(scope, id, StackProps.builder()
+                .stackName("Dev-Env-for-FCJ-in-Java-with-AWS")
+                .build());
 
         final Vpc devVpc = new Vpc(this, "dev-vpc");
 
+        final CfnParameter ideNameParam = new CfnParameter(this, "C9IDEName", CfnParameterProps.builder()
+                .description("Development environment for Faster Cryptography in Java with AWS")
+                .defaultValue("Faster Cryptography in Java with AWS")
+                .minLength(1)
+                .maxLength(60)
+                .build());
+
         new CfnEnvironmentEC2(this, "fcj-c9-ide",
                               CfnEnvironmentEC2Props.builder()
-                                      .name("Faster Cryptography in Java - Cloud9 IDE")
+                                      .name(ideNameParam.getValueAsString())
                                       .subnetId(devVpc.getPublicSubnets().iterator().next().getSubnetId())
                                       .instanceType("t3.medium")
                                       .automaticStopTimeMinutes(Duration.ofHours(4).toMinutes())
